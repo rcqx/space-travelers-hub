ad05d3939761cbeb0cf04779785fb55e0e11ac42
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-06-21 22:26:28 +0300

    fetchapi

---
+++ src/redux/rockets/api.js
@@ -0,0 +1,22 @@
+import SET_ROCKETS from './actiontypes';
+
+const fetchRockets = () => async (dispatch) => {
+  const response = await fetch('https://api.spacexdata.com/v3/rockets')
+    .catch((error) => error.message);
+  let fetchedData = await response.json();
+  fetchedData = fetchedData.map((rocket) => {
+    const obj = {
+      id: rocket.id,
+      flickr_images: rocket.flickr_images,
+      rocket_name: rocket.rocket_name,
+      description: rocket.description,
+    };
+    return obj;
+  });
+  dispatch({
+    type: SET_ROCKETS,
+    payload: fetchedData,
+  });
+};
+
+export default fetchRockets;
