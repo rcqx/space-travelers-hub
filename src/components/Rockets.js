ef7a5de193960ce1c08beab32cfb6dc23122f23e
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-06-22 03:04:49 +0300

    Add rockets component

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,9 +1,22 @@
+import Rocket from './Rocket';
+
 const Rockets = () => (
   <div>
-    <h1>ROCKETS</h1>
-    <p>Rocket information here</p>
+    <Rocket />
   </div>
-
 );
 
 export default Rockets;
+
+// const Rockets = () => {
+//   const rocketsArray = useSelector((state) => state.rockets);
+//   return (
+//     <div className="rockets-container">
+//       {rocketsArray.map((rocket) => (
+//         <div className="rocket-details" key={rocket.id}>
+//           <Rocket image={image} rocketName={rocketName} description={description} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
