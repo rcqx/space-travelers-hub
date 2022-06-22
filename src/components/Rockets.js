1ad0e63f943fdb715c1a7437f045023e08442504
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-06-22 03:10:17 +0300

    Modify rockets component

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -7,16 +7,3 @@ const Rockets = () => (
 );
 
 export default Rockets;
-
-// const Rockets = () => {
-//   const rocketsArray = useSelector((state) => state.rockets);
-//   return (
-//     <div className="rockets-container">
-//       {rocketsArray.map((rocket) => (
-//         <div className="rocket-details" key={rocket.id}>
-//           <Rocket image={image} rocketName={rocketName} description={description} />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
