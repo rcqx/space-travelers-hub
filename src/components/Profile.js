7916dcffbe830ea00fe7f53b29db80aaf2a3f887
jr-cast <castanos@pm.me>
2022-06-22 12:15:13 -0500

    Add mission counter

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,34 @@
-const Profile = () => (
-  <div className="profile-cont">
-    <div>
-      <h1>My Missions</h1>
-      <ul className="missionList">
-        Here you will see your appointed missions
-      </ul>
+import { useSelector } from 'react-redux';
+
+const Profile = () => {
+  const getMissions = useSelector((state) => state.missions);
+  const missions = getMissions;
+  let missionCounter = 0;
+
+  missions.map((item) => {
+    if (item.reserved) {
+      missionCounter += 1;
+    }
+    console.log(missionCounter);
+    return missionCounter;
+  });
+
+  return (
+    <div className="profile-cont">
+      <div>
+        <h1>My Missions</h1>
+        <ul className="missionList">
+          Here you will see your appointed missions
+        </ul>
+      </div>
+      <div>
+        <h1>My Rockets</h1>
+        <ul className="rocketList">
+          No reservations yet...
+        </ul>
+      </div>
     </div>
-    <div>
-      <h1>My Rockets</h1>
-      <ul className="rocketList">
-        No reservations yet...
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Profile;
