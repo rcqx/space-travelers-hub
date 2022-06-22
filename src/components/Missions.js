22c10373f2ad4395b874f5b8f6da2ceb29131da5
jr-cast <castanos@pm.me>
2022-06-21 19:41:48 -0500

    Update reserved property in missions object by clicking button

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,8 +1,11 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateReservation } from '../redux/missions/missions';
 
 const Missions = () => {
   const getMissions = useSelector((state) => state.missions);
   const missions = getMissions;
+  const dispatch = useDispatch();
+
   return (
     <div className="missionContainer">
       <table className="missionTable" cellSpacing="0">
@@ -19,8 +22,17 @@ const Missions = () => {
             <tr key={item.mission_id}>
               <td className="tdTitle">{item.mission_name}</td>
               <td>{item.description}</td>
-              <td><span>NOT A MEMBER</span></td>
-              <td className="btnCell"><button type="submit">Join Mission</button></td>
+              <td>
+                <span>NOT A MEMBER</span>
+              </td>
+              <td className="btnCell">
+                <button
+                  type="button"
+                  onClick={() => dispatch(updateReservation(item.mission_id))}
+                >
+                  Join Mission
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,7 +1,15 @@
+// actions
 const FETCH_API = 'space-travelers-hub/missions/FETCH_API';
+const UPDATE = 'space-travelers-hub/missions/UPDATE';
 const initialState = [];
 const baseMissionsUrl = 'https://api.spacexdata.com/v3/missions';
 
+// action creator
+export const updateReservation = (id) => (
+  { type: UPDATE, payload: id }
+);
+
+// action creators with thunk
 export const fetchMissions = () => async (dispatch) => {
   const connect = await fetch(baseMissionsUrl);
   const res = await connect.json().then((dataObject) => (dataObject));
@@ -10,6 +18,7 @@ export const fetchMissions = () => async (dispatch) => {
       mission_id: item.mission_id,
       mission_name: item.mission_name,
       description: item.description,
+      reserved: false,
     };
     return container;
   });
@@ -23,6 +32,13 @@ export default function reducerMissions(state = initialState, action) {
   switch (action.type) {
     case FETCH_API:
       return action.payload;
+    case UPDATE:
+      return [...state.map((item) => {
+        if (item.mission_id !== action.payload) {
+          return item;
+        }
+        return { ...item, reserved: !item.reserved };
+      })];
     default:
       return state;
   }
